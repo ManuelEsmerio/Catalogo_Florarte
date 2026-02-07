5cdc815f1646135aad2d1c0e8013975bba3c2672
Manuel Esmerio <manuel.esmerio1995@gmail.com>
2026-02-07 04:51:42 +0000

    message

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ const DeadlineBanner = () => {
   useEffect(() => {
     // Set the deadline for the next Valentine's Day.
     // Time is set for Tequila, Jalisco, Mexico (America/Mexico_City, which is UTC-6 during winter).
-    const deadline = new Date('2025-02-13T18:00:00-06:00');
+    const deadline = new Date('2026-02-13T18:00:00-06:00');
     const now = new Date();
     setIsAfterDeadline(now > deadline);
   }, []);
